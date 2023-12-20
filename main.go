2879c37380e6195531912accafb9957a9b45dfa0
Alan Urmancheev <alan.urman@gmail.com>
2023-12-21 00:39:29 +0400

    Make server look for configuration.json relative to its real path

--- main.go
+++ main.go
@@ -1,25 +1,51 @@
 package main
 
-import ("encoding/json"; "log"; "fmt"; "os"; "io")
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
 
-type configuration struct { Server string; Blacklist []string }
+type configuration struct {
+	Server    string
+	Blacklist []string
+}
+
+func readConfiguration() configuration {
+	var c configuration
+	var bytes []byte
+	{
+		executable, err := os.Executable()
+		if err != nil {
+			log.Fatal(err)
+		}
+		executable, err = filepath.EvalSymlinks(executable)
+		if err != nil {
+			log.Fatal(err)
+		}
+		path := filepath.Dir(executable) + "/configuration.json"
+		file, err := os.Open(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		bytes, err = io.ReadAll(file)
+	}
+
+	err := json.Unmarshal(
+		bytes,
+		&c,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return c
+}
 
 func main() {
-	var c configuration
-	{
-		var bytes []byte
-		{
-			file, err := os.Open("configuration.json")
-			if err != nil { log.Fatal(err) }
-			defer file.Close()
-			bytes, err = io.ReadAll(file)
-		}
-
-		err := json.Unmarshal(
-			bytes,
-			&c,
-		)
-		if err != nil { log.Fatal(err) }
-	}
+	c := readConfiguration()
 	fmt.Printf("%#v\n", c)
 }
